f146cf9b493596f58d4e141027223a7ed1b63aeb
daviddt <daviddentoom@gmail.com>
2013-09-08 22:15:34 +0200

    asd

---
+++ les 1/opdracth4.js
@@ -0,0 +1,44 @@
+var position = {
+	init: function() {
+
+	},
+	_start_interval: function(event) {
+
+	},
+	_update_position: function() {
+
+	},
+	_set_position: function(position) {
+
+	},
+	_check_locations: function(event) {
+
+	},
+	_calculate_distance: function(p1, p2) {
+
+	}
+}
+
+var map = {
+	generate_map: function(myOptions, canvasId) {
+
+	},
+	isNumber: function(n) {
+
+	},
+	update_positie: function(event) {
+
+	}
+}
+
+var debug {
+	_geo_error_handler: function(code, message) {
+
+	},
+	debug_message: function(message) {
+
+	},
+	set_custom_debugging: function(debugId) {
+		
+	}
+}
