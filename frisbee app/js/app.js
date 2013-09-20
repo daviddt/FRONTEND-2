1dcb2c2a7a055c75e06f4c52d0ad95a165b2434d
daviddt <daviddentoom@gmail.com>
2013-09-20 16:34:21 +0200

    fix

--- frisbee app/js/app.js
+++ frisbee app/js/app.js
@@ -2,12 +2,14 @@
 
 var FrisbeeApp = angular.module('frisbee', []);
 
-/* App Module */
+(function(){
 
-FrisbeeApp.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-      when('/schedule', {templateUrl: 'partials/schedule.html',   controller: 'ScheduleCtrl'}).
-      when('/game', {templateUrl: 'partials/game.html',   controller: 'GameCtrl'}).
-      when('/ranking', {templateUrl: 'partials/ranking.html',   controller: 'RankingCtrl'}).
-      otherwise({redirectTo: '/schedule'});
-}]);
+	FrisbeeApp.config(['$routeProvider', function ($routeProvider) {
+	  $routeProvider.
+	      when('/schedule', {templateUrl: 'partials/schedule.html',   controller: 'ScheduleCtrl'}).
+	      when('/game', {templateUrl: 'partials/game.html',   controller: 'GameCtrl'}).
+	      when('/ranking', {templateUrl: 'partials/ranking.html',   controller: 'RankingCtrl'}).
+	      otherwise({redirectTo: '/schedule'});
+	});
+
+}])();
